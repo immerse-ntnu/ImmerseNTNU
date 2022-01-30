7c2297d6cd1bcbb7e038f2d5bf7a3576a07d376e
candyshitter <kriss.1999@hotmail.com>
2022-01-30 21:45:49 +0100

    Made website ready for deployment

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,17 +1,22 @@
 let articleMenu = document.getElementById("topbar");
 
 const list = [
-  {text: "Showcasing", link: "showcase.html", icon: "SC"},
-  {text: "Events", link: "events.html", icon: "E"},
-  {text: "News", link: "news.html", icon: "N"},
-  {text: "About", link: "about.html", icon: "A"},
-  {text: "Sponsors", link: "sponsors.html", icon: "S"},
-  {text: "Contact", link: "contact.html", icon: "C"},
+  {text: "Showcasing", link: "showcase.html"},
+  {text: "Events", link: "events.html"},
+  {text: "News", link: "news.html"},
+  {text: "About", link: "about.html"},
+  {text: "Sponsors", link: "sponsors.html"},
 ];
 
-for(let n=0; n < list.length; n++) {
+for(let n = 0; n < list.length; n++) {
   let linkMenu = document.createElement("a");
   linkMenu.innerHTML = list[n].text;
   linkMenu.href = list[n].link;
   articleMenu.appendChild(linkMenu);
 }
+
+let linkMenu = document.createElement("a");
+linkMenu.innerHTML = "Apply Now!";
+linkMenu.href = "apply.html";
+linkMenu.classList.add("highlight");
+articleMenu.appendChild(linkMenu);

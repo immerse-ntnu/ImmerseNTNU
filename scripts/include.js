fa7bfa33838d73c8272a94a2a832c89a18ad3c74
kristley <kristley@users.noreply.github.com>
2023-08-20 13:42:35 +0000

    deploy: fef0418eb5939a2b10e7a52f2a9292d114cdab8e

---
+++ scripts/include.js
@@ -0,0 +1,14 @@
+let info=
+`<footer>
+    <p><a href="contact.html">Contact info:</a></p>
+    <p>Mail: immersentnu@gmail.com</p>
+    <p>Phone: +47 94421567</p>
+    <a href="https://www.facebook.com/immersentnu">Facebook<a>
+</footer>
+<div id="topbar">
+    <a href="index.html">
+        <img src="../img/immerse_white_text.png" style="margin-top: 18px;">
+    </a>
+    <div id="links"></div>
+</div>`; 
+document.write(info);

9a4a449f028b1a6ee5987904f919780fcd36d9f9
kristley <kristley@users.noreply.github.com>
2023-08-22 12:25:02 +0000

    deploy: 333f70e9c5cee7a691398d106e94a88cd07e1f5b

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const newsSection = document.querySelector('#news');
+newsSection.addEventListener('click', () => {
+  window.location.href = '/news/';
+});
+const eventSection = document.querySelector('#events');
+newsSection.addEventListener('click', () => {
+  window.location.href = '/events/';
+});
